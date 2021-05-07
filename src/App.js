a04384fc09821febd43e35415c9c45be85acd0a4
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-07 14:40:52 -0400

    have the header coponet set up to take a prop from the parent app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
+import Header from './components/Header'
+
 function App() {
 
-const name ='romo'
-  const x = true
-  const y = false
   return (
     <div className="container">
-       <h1>Hello</h1>
-       <h2>Welcome {name}</h2>
-       <h3>hello {1+1}</h3>
-       <h2> Hi {x ? name : 'not happening' }</h2>
-      <h2> Hi {y ? name : 'not happening'}</h2>
-       </div>
+     <Header title="Consider Phlebbas" />
+
+    </div>
   );
 }
 
