9500f87566bddc5269dc6450f32124f7dfa58839
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-08 12:53:16 -0400

    passing down an ondelete prop  to recieve an action from the child and pass it back to the app.js parent component. When all tasks are deleted it shows a message that there are no tasks, at the most child task.js we have an onclick action that triggers on delete which is in the parent app.js, this then sets the tasks state to show only the id that havent been selected by the on delete, correctly rendering the UI, state is immutable

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import Task from './Task.js'
 //Figuring out the limits of the parent component
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete}) => {
    
-    
-    
     return (
         <>
        
         {tasks.map((task) =>(
             <div>
-            <Task key={task.id} task={task} />
+            <Task key={task.id} task={task} onDelete={onDelete} />
             </div>
-            
         ))}
        
         </>
