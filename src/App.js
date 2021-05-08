89d3f7eb4ebbe7c8847d7847930a35ac5927fd4f
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-08 13:17:27 -0400

    added toggle reminder to the parent app, then sent an ontoggle prop down to tasks which maps it to the component task where can trigger an event using on double click and it will send that action to the tasks componet which will send it to the parent where it will trigger the on reminder function to be sent all the way to the most child component task where we will change the classname to interact with the css to give a green border at the left side

--- src/App.js
+++ src/App.js
@@ -22,17 +22,28 @@ function App() {
       day: '420',
       reminder: false,
     },
-
   ])
 
-    const deleteTask = (id) => {
-      setTasks(tasks.filter((task) => task.id !== id))
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
     }
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+      task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
 
   return (
     <div className="container">
     <Header />
-    {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No tasks to show') }
+    {tasks.length > 0 ? (
+    <Tasks tasks={tasks} 
+    onDelete={deleteTask} 
+    onToggle={toggleReminder} />
+    ) : (
+      'No tasks to show'
+      )}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Task from './Task.js'
 //Figuring out the limits of the parent component
-const Tasks = ({ tasks, onDelete}) => {
+const Tasks = ({ tasks, onDelete, onToggle}) => {
    
     return (
         <>
        
         {tasks.map((task) =>(
             <div>
-            <Task key={task.id} task={task} onDelete={onDelete} />
+            <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
             </div>
         ))}
        
