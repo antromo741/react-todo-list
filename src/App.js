359eadcb148ea852429d0f82add9a52c5cab5298
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-10 16:19:05 -0400

    added a delete request"

--- src/App.js
+++ src/App.js
@@ -26,14 +26,18 @@ function App() {
     return data
   }
 
-
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task }
     setTasks([...tasks, newTask])
   }
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
