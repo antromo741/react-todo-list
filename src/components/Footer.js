d22f2fef84f76c26a98283132c6e8c464c52dc64
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-10 17:23:54 -0400

    implimented use location and link to hide when the button appears to add a task

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Footer = () => {
     return (
         <footer>
             <p>Copyright &copy; 2021</p>
-            <a href="/about">about</a>
+            <Link To="/about">about</Link>
         </footer>
     )
 }
