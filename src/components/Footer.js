e3d40e87e7b777226b610da39251a0bf434abf05
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-10 17:16:00 -0400

    added a footer that brigns you to an about page and utilized the router package

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; 2021</p>
+            <a href="/about">about</a>
+        </footer>
+    )
+}
+
+export default Footer
